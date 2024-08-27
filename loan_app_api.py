e6d66a575b90e2e211060d35693831bdceb788d6
Shewalkar <shewalkarshubham@gmail.com>
2024-08-27 22:35:03 +0530

    result html file
    Added result html file for post request

--- loan_app_api.py
+++ loan_app_api.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_api():
-    return "<p>Good morning from Shubham</p>"
+@app.route('/')
+def index():
+    return render_template('result.html')
 
+@app.route('/submit', methods=['POST'])
+def submit():
+    # Extract form data
+    gender = request.form.get('gender')
+    married = request.form.get('married')
+    applicant_income = request.form.get('applicant_income')
+    loan_amount = request.form.get('loan_amount')
+    credit_history = request.form.get('credit_history')
+    
+    # Render the result HTML page with the submitted data
+    return render_template('result.html',
+                           gender=gender,
+                           married=married,
+                           applicant_income=applicant_income,
+                           loan_amount=loan_amount,
+                           credit_history=credit_history)
 
-if __name__ == "__main__":
-    app.run(debug = True)
+if __name__ == '__main__':
+    app.run(debug=True)
